895633b5e96e326ce04c049ed1dd0cc66152586a
housemeow <housemeow@gmail.com>
2025-02-27 17:34:22 +0800

    feat: update layout

--- hair/src/components/PrepareView/PrepareView.vue
+++ hair/src/components/PrepareView/PrepareView.vue
@@ -18,16 +18,18 @@ onMounted(() => {
 
 <template>
   <div class="prepare-view">
-    <label class="fixed top-[2em] left-[1em] z-50" for="error"><input id="error" type="checkbox" v-model="store.fileError">Error</label>
-    <img
-      src="@/assets/logo.webp"
-      srcset="@/assets/logo@2x.webp 2x,
-              @/assets/logo@3x.webp 3x">
-    <h1>歡迎使用安夏朵智慧Ai髮色系統</h1>
-    <p>最適合亞洲人的矯/補色洗髮精 <br/>隨心所欲玩轉髮色 So easy</p>
-    <LoadingProgress v-if="store.loadingVisible"/>
-    <PictureSelectButton v-else @change="handleFileSelect" />
-    <ErrorView v-if="store.fileError" />
+    <main>
+      <label class="fixed top-[2em] left-[1em] z-50" for="error"><input id="error" type="checkbox" v-model="store.fileError">Error</label>
+      <img
+        src="@/assets/logo.webp"
+        srcset="@/assets/logo@2x.webp 2x,
+                @/assets/logo@3x.webp 3x">
+      <h1>歡迎使用安夏朵智慧Ai髮色系統</h1>
+      <p>最適合亞洲人的矯/補色洗髮精 <br/>隨心所欲玩轉髮色 So easy</p>
+      <LoadingProgress v-if="store.loadingVisible"/>
+      <PictureSelectButton v-else @change="handleFileSelect" />
+      <ErrorView v-if="store.fileError" />
+    </main>
   </div>
 </template>
 
@@ -35,80 +37,100 @@ onMounted(() => {
 .prepare-view {
   flex: 1;
   display: flex;
-  padding-top: 144px;
-  padding-bottom: 124px;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
   position: relative;
+  padding-top: 40px;
+  padding-bottom: 40px;
 
-  img {
-    width: 140px;
-  }
-
-  h1 {
-    margin-top: 60px;
-    font-size: 17px;
-    font-weight: 500;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: 1.47;
-    letter-spacing: normal;
-    text-align: center;
-    color: #707070;
-  }
-
-  p {
-    margin-top: 10px;
-    font-size: 13px;
-    font-weight: normal;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: 1.54;
-    letter-spacing: normal;
-    text-align: center;
-    color: #707070;
-  }
-
-  .loading-progress {
-    margin-top: auto;
-  }
-
-  .picture-select-button {
-    margin-top: auto;
-    margin-bottom: -13px;
-  }
-}
-
-@media screen and (min-width: 600px) {
-  .prepare-view {
-    padding-top: 215px;
-    padding-bottom: 220px;
+  main {
+    flex: 1;
+    margin-top: -20px;
+    max-height: 492px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
     img {
-      width: 160px;
+      width: 140px;
     }
 
     h1 {
-      margin-top: 68px;
+      margin-top: 60px;
       font-size: 17px;
       font-weight: 500;
+      font-stretch: normal;
+      font-style: normal;
       line-height: 1.47;
+      letter-spacing: normal;
+      text-align: center;
+      color: #707070;
     }
 
     p {
       margin-top: 10px;
       font-size: 13px;
+      font-weight: normal;
+      font-stretch: normal;
+      font-style: normal;
+      line-height: 1.54;
+      letter-spacing: normal;
+      text-align: center;
+      color: #707070;
+    }
+
+    .loading-progress {
+      margin-top: auto;
+    }
+
+    .picture-select-button {
+      margin-top: auto;
+      margin-bottom: -13px;
+    }
+  }
+}
+
+@media screen and (min-width: 600px) {
+  .prepare-view {
+    padding-top: 75px;
+    padding-bottom: 75px;
+
+    main {
+      margin-top: -55px;
+      max-height: 472px;
+
+      img {
+        width: 160px;
+      }
+
+      h1 {
+        margin-top: 68px;
+        font-size: 17px;
+        font-weight: 500;
+        line-height: 1.47;
+      }
+
+      p {
+        margin-top: 10px;
+        font-size: 13px;
+      }
     }
   }
 }
 
 @media screen and (min-width: 992px) {
   .prepare-view {
-    padding-top: 238px;
-    padding-bottom: 200px;
+    padding-top: 47px;
+    padding-bottom: 47px;
 
-    h1 {
-      margin-top: 60px;
+    main {
+      margin-top: -17px;
+      max-height: 524px;
+
+      h1 {
+        margin-top: 60px;
+      }
     }
   }
 }
